35fcbec8ceb0ee428a85f70b6a7eb3c1dc9d8613
viraj <viraj.ag007@gmail.com>
2020-12-20 15:58:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/", (req, res) => {
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fname,
+          LNAME: lname,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  console.log(jsonData);
+  const url = process.env.URI;
+  const options = {
+    method: "POST",
+    auth: process.env.AUTH,
+  };
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.send("Succesfully Subscribed");
+    } else {
+      res.send("There was an error");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running`);
+});
